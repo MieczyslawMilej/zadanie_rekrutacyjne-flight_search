d31ffb9cd3f23a0308e1772c982dd9f96c81f3d0
Mieczysław <38013509+MieczyslawMilej@users.noreply.github.com>
2020-08-28 17:10:18 +0200

    Cleaning app.js file

--- src/App.js
+++ src/App.js
@@ -1,38 +1,9 @@
 import React from "react";
 import "./scss/main.scss";
 import Form from "./components/Form";
-// import Flights from "./components/Flights";
 import { ReactComponent as Airplane } from "./airplane.svg";
 
 function App() {
-	///////////////////////////////////// DFS method
-
-	// function dfs(start, finish, visited = new Set()) {
-	//   console.log(start);
-	//   // debugger
-
-	//   visited.add(start);
-
-	//   const destinations = adjacencyList.get(start);
-
-	//   const path = [];
-
-	//   for (const destination of destinations) {
-	//     // console.log(destination);
-
-	//     if (destination === finish) {
-	//       console.log(` found =====>${finish}`);
-	//       console.log(visited);
-	//       return visited;
-	//     }
-
-	//     if (!visited.has(destination)) {
-	//       dfs(destination, finish, visited);
-	//     }
-	//   }
-	// }
-
-	// dfs("BTS", "DUB");
 
 	return (
 		<div className="App">
